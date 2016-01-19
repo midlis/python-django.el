b523f426f2074100d2452278a753b99c4ed5c401
Milan Broum <milanb@google.com>
2016-01-19 08:42:31 +0000

    Enforce absolute path. TODO: investigate alternatives.

--- python-django.el
+++ python-django.el
@@ -683,6 +683,8 @@ else:
     # Django>=1.7 app loading.
     apps.populate(installed_apps=settings.INSTALLED_APPS)
     app_paths = {app.label: app.path for app in apps.get_app_configs()}
+for k,v in app_paths.items():
+    app_paths[k] = os.path.abspath(v)
 print(json.dumps(app_paths), end='')")))))
     python-django-info--get-app-paths-cache))
 
